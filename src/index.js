226a98faa12f58d97e3ca16a4565bfd08c13c77e
AlenaShkr <elalshkr@gmail.com>
2019-03-17 02:16:41 +0300

    brackets:Implement function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let copyStr = str;
+  const configLen = bracketsConfig.length;
+  
+  for ( let i = 0; i < copyStr.length;  i++) {
+    for (let j = 0; j < configLen; j++){
+      
+     if ((copyStr[i] === bracketsConfig[j][0]) && (copyStr[i + 1] === bracketsConfig[j][1])){
+
+        let reg = bracketsConfig[j][0] + bracketsConfig[j][1];
+        copyStr = copyStr.replace(reg,'');
+
+        i = i-1;
+        j = -1;
+      }
+    }
+  }
+
+  return copyStr === '';
 }
